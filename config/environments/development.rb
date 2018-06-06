a9d7d397384e9a57369248e59abd64711d98cbbf
Unknown <cristian.fernandez@usach.cl>
2018-06-06 17:18:01 -0400

    Arreglada funcionalidad de restablecer contraseña

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,9 +28,23 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  #config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.perform_caching = false
+  #config.action_mailer.perform_caching = false
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: "localhost:3000" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "mail.gmail.com:80",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "transbeauth@gmail.com",
+    password: "transbeapp"
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
